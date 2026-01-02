5df2ed7fea18b1ae38c59c01d5f89bc4270227ae
BaisayanB <bbbbaisayan@gmail.com>
2026-01-02 20:34:16 +0530

    implemented authentication through better route, updated schema with auth tables and added protectedprocedures in trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-"use client";
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
 
-import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+const Page = async () => {
+  await requireAuth();
 
-const Page = () => {
-  const trpc = useTRPC();
-  const { data: users } = useQuery(trpc.getUsers.queryOptions());
+  const data = await caller.getUsers();
 
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      {JSON.stringify(users)}
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+      protected server component
+      <div>{JSON.stringify(data, null, 2)}</div>
     </div>
   );
 };
