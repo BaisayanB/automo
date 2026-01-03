a333f54e1d82068628523bc8c0fb386d9220316b
BaisayanB <bbbbaisayan@gmail.com>
2026-01-03 15:14:25 +0530

    setup inngest done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,31 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { requireAuth } from "@/lib/auth-utils";
+import { useTRPC } from "@/trpc/client";
 import { caller } from "@/trpc/server";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const data = await caller.getUsers();
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        queryClient.invalidateQueries(trpc.getWorkflows.queryOptions());
+      },
+    })
+  );
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       protected server component
       <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create workflow
+      </Button>
     </div>
   );
 };
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,15 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany({
-      where: {
-        id: ctx.auth.user.id,
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+
+    return prisma.workflow.create({
+      data: {
+        name: "test-workflow",
       },
     });
   }),
