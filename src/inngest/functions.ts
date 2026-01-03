54060f3881101e77af89b737a7fe0a85228d0d67
BaisayanB <bbbbaisayan@gmail.com>
2026-01-03 19:58:10 +0530

    added ai-sdk, and integrated openai, gemini n claude with it

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,51 @@
 import { inngest } from "./client";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai";
+import { createAnthropic } from "@ai-sdk/anthropic";
+import { generateText } from "ai";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+const google = createGoogleGenerativeAI();
+const openai = createOpenAI();
+const anthropic = createAnthropic();
+
+export const execute = inngest.createFunction(
+  { id: "execute-ai" },
+  { event: "execute/ai" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "1s");
-    return { message: `Hello ${event.data.email}!` };
+    const { steps: geminiSteps } = await step.ai.wrap(
+      "gemini-generate-text",
+      generateText,
+      {
+        model: google("gemini-2.5-flash"),
+        system: "You are a helpful assistant.",
+        prompt: "What is photosynthesis",
+      }
+    );
+
+    const { steps: openaiSteps } = await step.ai.wrap(
+      "openai-generate-text",
+      generateText,
+      {
+        model: openai("gpt-5"),
+        system: "You are a helpful assistant.",
+        prompt: "What is photosynthesis",
+      }
+    );
+
+    const { steps: anthropicSteps } = await step.ai.wrap(
+      "anthropic-generate-text",
+      generateText,
+      {
+        model: anthropic("claude-sonnet-4-5"),
+        system: "You are a helpful assistant.",
+        prompt: "What is photosynthesis",
+      }
+    );
+
+    return {
+      geminiSteps,
+      openaiSteps,
+      anthropicSteps,
+    };
   }
 );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,21 @@
+import { inngest } from "@/inngest/client";
 import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
+import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
 
 export const appRouter = createTRPCRouter({
+  testAi: protectedProcedure.mutation(async () => {
+    await inngest.send({ name: "execute/ai" });
+
+    return { success: true, message: "Job queued" };
+  }),
+
   getWorkflows: protectedProcedure.query(({ ctx }) => {
     return prisma.workflow.findMany();
   }),
-  createWorkflow: protectedProcedure.mutation(async () => {
 
+  createWorkflow: protectedProcedure.mutation(async () => {
     return prisma.workflow.create({
       data: {
         name: "test-workflow",
