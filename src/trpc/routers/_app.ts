99e82133be051d5d1f293ebb256be5df9927d216
BaisayanB <bbbbaisayan@gmail.com>
2026-01-05 21:00:04 +0530

    created some new routes to test in server folder

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,27 +1,9 @@
-import { inngest } from "@/inngest/client";
-import { createTRPCRouter, protectedProcedure } from "../init";
-import prisma from "@/lib/db";
-import { google } from "@ai-sdk/google";
-import { generateText } from "ai";
+import { createTRPCRouter } from "../init";
+import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-  testAi: protectedProcedure.mutation(async () => {
-    await inngest.send({ name: "execute/ai" });
+  workflowsRouter: workflowsRouter,
 
-    return { success: true, message: "Job queued" };
-  }),
-
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany();
-  }),
-
-  createWorkflow: protectedProcedure.mutation(async () => {
-    return prisma.workflow.create({
-      data: {
-        name: "test-workflow",
-      },
-    });
-  }),
 });
 
 // export type definition of API
