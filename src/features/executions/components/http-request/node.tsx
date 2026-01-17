59a4981a39f246fd78cecf695388f1621ffdf3c9
BaisayanB <bbbbaisayan@gmail.com>
2026-01-17 13:59:15 +0530

    improved props for httprequest dialog

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -4,22 +4,12 @@ import { type NodeProps, type Node, useReactFlow } from "@xyflow/react";
 import { BaseExecutionNode } from "../base-execution-node";
 import { GlobeIcon } from "lucide-react";
 import { memo, useState } from "react";
-import { HttpRequestDialog } from "./dialog";
-// import {
-//   output,
-//   ZodObject,
-//   ZodURL,
-//   ZodEnum,
-//   ZodOptional,
-//   ZodString,
-// } from "zod";
-import { FormType } from "./dialog";
+import { HttpRequestDialog, HttpRequestFormValues } from "./dialog";
 
 type HttpRequestNodeData = {
   endpoint?: string;
   method?: "GET" | "POST" | "PATCH" | "PUT" | "DELETE";
   body?: string;
-  [key: string]: unknown;
 };
 
 type HttpRequestNodeType = Node<HttpRequestNodeData>;
@@ -31,7 +21,7 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
   const nodeStatus = "initial";
   const handleOpenSettings = () => setDialogOpen(true);
 
-  const handleSubmit = (values: FormType) => {
+  const handleSubmit = (values: HttpRequestFormValues) => {
     setNodes((nodes) =>
       nodes.map((node) => {
         if (node.id === props.id) {
@@ -39,14 +29,12 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
             ...node,
             data: {
               ...node.data,
-              endpoint: values.endpoint,
-              method: values.method,
-              body: values.body,
+              ...values,
             },
           };
         }
         return node;
-      })
+      }),
     );
     setDialogOpen(false);
   };
@@ -62,9 +50,7 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
         open={dialogOpen}
         onOpenChange={setDialogOpen}
         onSubmit={handleSubmit}
-        defaultEndpoint={nodeData.endpoint}
-        defaultMethod={nodeData.method}
-        defaultBody={nodeData.body}
+        defaultValues={nodeData}
       />
       <BaseExecutionNode
         {...props}
